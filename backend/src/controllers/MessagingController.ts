c907445f8058887febf7fd5cfd6c14a01db0c759
lumi-sg <shaicloud@gmail.com>
2024-01-13 00:28:28 -0500

    changed delete conversation to use user object not only ID

---
+++ backend/src/controllers/MessagingController.ts
@@ -0,0 +1,164 @@
+import express, { response } from "express";
+import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+import { UserType, User } from "../models/User";
+import { Message } from "../models/Message";
+import { Conversation } from "../models/Conversation";
+
+export const get_conversations = asyncHandler(
+	async (req: express.Request, res: express.Response) => {
+		try {
+			if (!req.user) {
+				return;
+			}
+
+			const user = await User.findById((req.user as any).userId).select(
+				"-password"
+			);
+			if (!user) {
+				console.log("User not found");
+				res.status(404).send("User not found");
+				return;
+			}
+			const conversations = await Conversation.find({
+				participants: user._id,
+			});
+			console.log(`Found ${conversations.length} conversations`);
+			res.status(200).json(conversations);
+		} catch (error: any) {
+			console.error("Error getting conversations", error);
+			res.status(500).send(
+				`Error getting  conversations: ${error.message}`
+			);
+		}
+	}
+);
+
+export const get_conversation = asyncHandler(
+	async (req: express.Request, res: express.Response) => {
+		try {
+			if (!req.user) {
+				return;
+			}
+			const user = await User.findById((req.user as any).userId).select(
+				"-password"
+			);
+			if (!user) {
+				console.log("User not found");
+				res.status(404).send("User not found");
+				return;
+			}
+			const conversation = await Conversation.findById(req.params.id);
+			if (!conversation) {
+				console.log("Conversation not found");
+				res.status(404).send("Conversation not found");
+				return;
+			}
+			res.status(200).json(conversation);
+		} catch (error: any) {
+			console.error("Error getting conversation", error);
+			res.status(500).send(
+				`Error getting conversation: ${error.message}`
+			);
+		}
+	}
+);
+
+export const create_conversation = [
+	body("conversationTitle")
+		.trim()
+		.notEmpty()
+		.withMessage("Please enter a conversation title")
+		.isLength({ max: 50 })
+		.withMessage("Conversation title must be 50 characters or less")
+		.isLength({ min: 1 })
+		.withMessage("Conversation title must be at least 1 character long"),
+	asyncHandler(async (req: express.Request, res: express.Response) => {
+		try {
+			const errors = validationResult(req);
+			const { conversationTitle, senderUserID, receiverUserID } =
+				req.body;
+			if (!errors.isEmpty()) {
+				console.log("Validation errors", errors.array());
+				res.status(400).send(errors);
+				return;
+			}
+
+			if (!senderUserID || !receiverUserID) {
+				console.log("senderUserID or receiverUserID not provided");
+				res.status(400).send(
+					"senderUserID or receiverUserID not provided"
+				);
+				return;
+			}
+
+			if (senderUserID === receiverUserID) {
+				console.log(
+					"senderUserID and receiverUserID must be different"
+				);
+				res.status(400).send(
+					"senderUserID and receiverUserID must be different"
+				);
+				return;
+			}
+
+			const conversation = new Conversation({
+				conversationTitle,
+				participants: [senderUserID, receiverUserID],
+			});
+			await conversation.save();
+			console.log(
+				`Conversation with participants ${senderUserID} and ${receiverUserID}created with ID ${conversation._id}`
+			);
+			res.status(201).send("Conversation created");
+
+			if (!req.user) {
+				return;
+			}
+		} catch (error: any) {
+			console.error("Error creating conversation", error);
+			res.status(500).send(
+				`Error creating conversation: ${error.message}`
+			);
+			return;
+		}
+	}),
+];
+
+export const delete_conversation = asyncHandler(
+	async (req: express.Request, res: express.Response) => {
+		try {
+			const { userID, conversationID } = req.body;
+			if (!userID) {
+				console.log("User to be removed not provided");
+				res.status(400).send("User to be removed not provided");
+				return;
+			}
+
+			if (!conversationID) {
+				console.log("Conversation not found");
+				res.status(404).send("Conversation not found");
+				return;
+			}
+
+			const user = await User.findById(userID).select("-password");
+			if (!user) {
+				console.log("User not found");
+				res.status(404).send("User not found");
+				return;
+			}
+
+			await Conversation.findByIdAndUpdate(
+				conversationID,
+				{ $pull: { participants: user } },
+				{ new: true }
+			);
+			res.status(200).send(`User ${user} removed from conversation`);
+		} catch (error: any) {
+			console.error("Error deleting conversation", error);
+			res.status(500).send(
+				`Error deleting conversation: ${error.message}`
+			);
+		}
+	}
+);
