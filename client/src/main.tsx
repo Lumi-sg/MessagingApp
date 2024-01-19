722b6496b306359506d8bfee6a3cad2727c5b8d7
lumi-sg <shaicloud@gmail.com>
2024-01-19 02:06:27 -0500

    recycled past pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+	createBrowserRouter,
+	Navigate,
+	RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./components/ErrorPage/ErrorPage.tsx";
+import Loading from "./components/Loading/Loading.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export const BASEURL = "http://localhost:3000";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Navigate to="/index" />,
+	},
+	{
+		path: "/index",
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider
+			router={router}
+			fallbackElement={<Loading />}
+			future={{ v7_startTransition: true }}
+		/>
+	</React.StrictMode>
+);
