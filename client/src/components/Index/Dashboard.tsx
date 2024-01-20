1eb95159f606ced253322c7d66281fec7b809643
lumi-sg <shaicloud@gmail.com>
2024-01-19 20:50:24 -0500

    added containers for layout

--- client/src/components/Index/Dashboard.tsx
+++ client/src/components/Index/Dashboard.tsx
@@ -1,21 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useUserStore } from "../../stores/userStore";
 import Footer from "../Footer/Footer";
 import { useRouteLoaderData } from "react-router-dom";
+import "./dashboard.css";
 
 const Dashboard = () => {
 	const { user } = useUserStore();
-  const response = useRouteLoaderData("conversations");
+	const response = useRouteLoaderData("conversations");
 
 	return (
-		<div className="mainContainer">
-			<div className="dashboardContainer">
-				<div className="greetingContainer">
-					<h1>Welcome {user!.username}</h1>
-          
-				</div>
-			</div>
-			<Footer />
+		<div className="dashboardContainer">
+			<div className="sideBarContainer">
+        <div className="topRowContainer">
+          <button>Logout</button>
+          <h1>{user!.username}</h1>
+          <button>+</button>
+        </div>
+      </div>
+			<div className="conversationContainer">
+        <div className="conversationtopRowContainer"></div>
+      </div>
 		</div>
 	);
 };
